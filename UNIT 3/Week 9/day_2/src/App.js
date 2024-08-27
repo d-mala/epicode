b00d5cc2a60e1f1ed63b8918a6f604b65d84365d
Davide Malavenda <davide.malavenda26@gmail.com>
2024-08-27 20:26:25 +0200

    es unit 3

---
+++ UNIT 3/Week 9/day_2/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MyNav from './components/MyNa.jsx';
+
+function App() {
+  return (
+    <>
+    <MyNav />
+    </>
+  );
+}
+
+export default App;
